2a1cfd03225d12634a55f9d76d79ffacc718f1bb
oladips <oladips200@gmail.com>
2022-04-09 14:38:37 +0000

    created sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,42 @@
-import React from 'react'
+/** @format */
+import React from "react";
+import { NavLink } from "react-router-dom";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import PeopleAltOutlinedIcon from "@mui/icons-material/PeopleAltOutlined";
+import PersonPinOutlinedIcon from "@mui/icons-material/PersonPinOutlined";
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
-  )
+    <div className="h-[100vh] bg-black w-[220px] fixed ">
+      <div className="text-center justify-center">
+        <img
+          src="/images/site-logo.png"
+          className="object-contain ml-6 "
+          alt=""
+        />
+      </div>
+      <div className="mt-6">
+        <div className="my-3 border-l-4 border-green-400 hover:text-green-500">
+          <NavLink to="/" className="flex items-center hover:text-green-500">
+            <GridViewOutlinedIcon className="mx-4 text-green-400 text-lg" />
+            <p className="text-green-400 text-lg">Dashboard</p>
+          </NavLink>
+        </div>
+        <div className="my-3">
+          <NavLink to="/" className="flex items-center">
+            <PeopleAltOutlinedIcon className="mx-4 text-white text-lg" />
+            <p className="text-white text-lg">Recruitment</p>
+          </NavLink>
+        </div>
+        <div className="mx-y">
+          <NavLink to="/" className="flex items-center">
+            <PersonPinOutlinedIcon className="mx-4 text-white text-lg" />
+            <p className="text-white text-lg">Staffs</p>
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import Sidebar from '../components/Sidebar';
-import HomeHeader from '../components/HomeHeader';
+/** @format */
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import HomeHeader from "../components/HomeHeader";
 
 function HomeScreen() {
   return (
-      <div>
-          <div className="">
-              <Sidebar />
-              <HomeHeader />
-          </div>
+    <div>
+      <div className="flex justify-between">
+        <Sidebar />
+              <div className="home-body">
+                  
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default HomeScreen;
