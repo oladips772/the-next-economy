e896960a60cfe951eaf4a6982212d236827291c6
oladips <oladips200@gmail.com>
2022-04-24 19:39:49 +0000

    creating get admins and get entrepreneurs routes in server

--- server/server.js
+++ server/server.js
@@ -2,10 +2,29 @@
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
+const { admins } = require("./data/admins");
 
 const PORT = process.env.PORT || 4000;
+const app = express();
 
-const app = express.Router();
-app.get("/", () => {
-  console.log(`server running on ${PORT}`);
+app.get("/", (req, res) => {
+  res.send(`server is running on port ${PORT}`);
 });
+
+// get admins from server
+app.get("/api/admins", (req, res) => {
+  res.json(admins);
+});
+
+// get admin by id
+app.get("/api/admins/:id", (req, res) => {
+  const { id } = req.params;
+  const admin = admins.find((admin) => admin.id === parseInt(id));
+  if (!admin) {
+    res.status(404).json({ msg: "admin not found" });
+  } else {
+    res.json(admin);
+  }
+});
+
+app.listen(PORT, console.log(`server is running on port ${PORT}`));
